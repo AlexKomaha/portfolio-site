c7e11bf4ea8ceb4b45ab8b2eed36800003647712
AlexKomaha <107383667+AlexKomaha@users.noreply.github.com>
2025-01-16 13:39:50 +0900

    Added index page

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,13 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex items-center h-screen relative bg-cover bg-[url('/assets/bg-explosion.png')]">
-      <div className="absolute right-10 md:right-40 bottom-0 z-[10]/">
+      <div className="absolute right-10 md:right-40 bottom-0 z-[10]">
         <Image
           src='/assets/MoustacheMan.png'
           alt="MoustacheMan"
           width={560}
           height={560}
-          className="w-[300px] h-[300px] z-[1] md:w-[560px]/="
+          className="w-[300px] h-[500px] z-[1] md:h-[560px] md:w-[560px]"
         />
       </div>
         <Image
@@ -19,6 +19,23 @@ export default function Home() {
           height={230}
           className="absolute left-0 top-0"
         />
+        <div className="flex flex-col gap-3 z-[10] pl-40 pt-20">
+          <h1 className="text-[50px] text-white max-w-[500px]">
+            Transforming ideas into <span className="text-red-500">Digital Reality</span>
+          </h1>
+          <p className="text-[14px] text-gray-200 md:text-gray-400 mb-10 md:pb-2 max-w-[400px]">
+          In a world where technology drives success, transforming ideas into digital solutions has become an art form. We specialize in bringing bold concepts to life, crafting innovative products that redefine possibilities.
+          </p>
+          <div className="relative w-[100px] h-[100px] md:w-[160px] md:h-[160px] mt-5">
+            <Image src='/assets/rounded-text.png'
+            alt="projects"
+            width={160}
+            height={160}
+            className="slow-spin"
+            />
+          </div>
+
+        </div>
     </main>
   );
 }
