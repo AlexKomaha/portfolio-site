1fb3d1f5df9aa03153ee1b04b0a638cd03bff439
AlexKomaha <107383667+AlexKomaha@users.noreply.github.com>
2025-01-27 13:07:23 +0900

    Added my projects and slider

--- app/my-projects/page.tsx
+++ app/my-projects/page.tsx
@@ -2,7 +2,17 @@ import React from "react"
 
 const Page = () => {
   return (
-    <div>My Projects</div>
+    <div className="flex items-center justify-center gap-5 flex-col md:flex-row h-screen bg-[#0C011A]">
+      <div className="flex flex-col gap-3">
+        <h1 className="text-[50px] text-white font-semibold">
+          My Work <span className="text-red-500">.</span>
+        </h1>
+        <p className="max-w-[400px] text-[16px] text-gray-200 md:text-gray-400">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio, natus adipisci quis quae perspiciatis maxime ad fugit similique aut molestias animi minus assumenda quos odit eum repudiandae illum dicta nesciunt!
+        </p>
+      </div>
+      {/* <projectSlider /> */}
+    </div>
   )
 }
 
