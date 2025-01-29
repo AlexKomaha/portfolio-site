7a937eb8b73285cb158ef910c1204d6d739707bf
AlexKomaha <107383667+AlexKomaha@users.noreply.github.com>
2025-01-29 10:08:19 +0900

    Added service update

--- components/serviceSlider.tsx
+++ components/serviceSlider.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import "swiper/css";
@@ -13,11 +12,11 @@ import Image from "next/image";
 const ServiceSlider = () => {
   return (
     <div className="absolute bottom-0 right-20 md:bottom-40 md:right-30 w-[60%] md:w-[55%]">
-            <Swiper
+      <Swiper
         breakpoints={{
           340: {
             slidesPerView: 1,
-            spaceBetween: 15
+            spaceBetween: 15,
           },
           700: {
             slidesPerView: 3,
@@ -32,18 +31,23 @@ const ServiceSlider = () => {
       >
         {ServiceData.map((item) => (
           <SwiperSlide key={item.title}>
-            <div>
-
+            <div className="rounded-md cursor-pointer flex flex-col gap-6 mb-10 group relative text-white shadow-lg rounde-xl px-6 py-8 h-[300px] w-[250px] overflow-hidden">
+              <div
+                className="absolute inset-0 bg-cover bg-center"
+                style={{ backgroundImage: `url(${item.backgroundImage})` }}
+              />
+              <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-50" />
+              <div className="relative flex flex-col gap-3">
+                <item.icon className="text-blue-600  group-hover:text-blue-400 w-[32px] h-[32px]" />
+                <h1 className="text-xl lg:text-2xl">{item.title}</h1>
+                <p className="lg:text-[18px]">{item.content}</p>
+              </div>
             </div>
           </SwiperSlide>
         ))}
-
       </Swiper>
-
-
     </div>
-  )
+  );
+};
 
-}
-
-export default ServiceSlider
+export default ServiceSlider;
